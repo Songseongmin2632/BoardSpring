3fae3893db88a71baac189d8ffee55f1c2e5a390
SeongMin <skj041013@naver.com>
2023-11-22 15:13:16 +0900

    "test"

---
+++ board-spring/src/main/java/com/example/demo/board/domain/Board.java
@@ -0,0 +1,25 @@
+package com.example.demo.board.domain;
+
+import java.sql.Timestamp;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class Board {
+  private int id;
+  @NonNull
+  private String title;
+  @NonNull
+  private String content;
+  private int views = 0;
+  private int likes = 0;
+  private int hates = 0;
+  private Timestamp createdAt;
+  private boolean isWithdrew = false;
+  @NonNull
+  private int userId;
+}
